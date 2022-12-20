2ce95349f290dd5be7ac03835c899faa127bcc21
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-20 09:21:47 +0100

    fin js + initialisation scss

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,29 @@ function newQuote() {
       .then((data) => {
         const blockquote = document.createElement('blockquote');
         blockquote.textContent = data.quote
-        document.body.appendChild(blockquote);
 
         const divAutor = document.createElement('p');
         divAutor.textContent = data.author;
-        document.body.appendChild(divAutor);
 
         const pPerma = document.createElement('a');
         const linkText = document.createTextNode("Lien vers la citation.")
         pPerma.appendChild(linkText);
         pPerma.title = "Lien vers la citation.";
         pPerma.href = data.permalink;
-        document.body.appendChild(pPerma);
-
+        
         const imgPhoto = document.createElement('img');
         imgPhoto.src = data.photo;
-        document.body.appendChild(imgPhoto);
+
+        const divContent = document.getElementById('content');  
+        
+        const divTout = document.getElementById('tout');
+
         console.log(data);
+        divContent.appendChild(imgPhoto);
+        divContent.appendChild(blockquote);
+        divContent.appendChild(divAutor);
+        divContent.appendChild(pPerma);
+        divTout.appendChild(divContent)
       })
       .catch((error) => {
         console.log("There was an error!", error);
